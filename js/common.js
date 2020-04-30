59bdbd7739cbb49d8b8008af020fa8113f5c6126
109611gg@gmail.com <109611gg@gmail.com>
2020-04-30 15:12:12 +0800

    'upload'

--- js/common.js
+++ js/common.js
@@ -1,20 +1,22 @@
 /*
  * @Author: XiaoMing
  * @Date: 2020-04-30 10:38:55 
- * @Last Modified by:   XiaoMing 
- * @Last Modified time: 2020-04-30 10:38:55 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2020-04-30 14:51:27
  */
 
-//右边栏
-var sidebarright = document.querySelector('.sidebar_right');
-var like = document.querySelector('.like');
-var likeTop = like.offsetTop;
+window.addEventListener('load', function () {
+    //右边栏
+    var sidebarright = document.querySelector('.sidebar_right');
+    var like = document.querySelector('.like');
+    var likeTop = like.offsetTop;
 
-document.addEventListener('scroll', function () {
-    if (window.pageYOffset >= likeTop) {
-        sidebarright.style.display = 'block';
-    } else {
-        sidebarright.style.display = 'none';
-    }
+    document.addEventListener('scroll', function () {
+        if (window.pageYOffset >= likeTop) {
+            sidebarright.style.display = 'block';
+        } else {
+            sidebarright.style.display = 'none';
+        }
 
+    })
 })
--- js/index.js
+++ js/index.js
@@ -2,23 +2,25 @@
  * @Author: XiaoMing
  * @Date: 2020-04-29 22:54:02 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2020-04-30 10:38:34
+ * @Last Modified time: 2020-04-30 14:51:23
  */
 
-//左侧边栏
-var sidebarLeft = document.querySelector('.sidebar_left');
-var main = document.querySelector('.main');
-var mainTop = main.offsetTop;   //获取main距离浏览器顶部的top值
-var sidebarLeftTop = sidebarLeft.offsetTop - mainTop;//等于sidebarLeft距离main顶部的值
+window.addEventListener('load', function () {
+    //左侧边栏
+    var sidebarLeft = document.querySelector('.sidebar_left');
+    var main = document.querySelector('.main');
+    var mainTop = main.offsetTop;   //获取main距离浏览器顶部的top值
+    var sidebarLeftTop = sidebarLeft.offsetTop - mainTop;//等于sidebarLeft距离main顶部的值
 
-//页面滚动事件
-document.addEventListener('scroll', function () {
-    //window.pageYOffset:获取页面被卷去的高度
-    if (window.pageYOffset >= mainTop) {
-        sidebarLeft.style.position = 'fixed'
-        sidebarLeft.style.top = sidebarLeftTop + 'px';
-    } else {
-        sidebarLeft.style.position = 'absolute'
-        sidebarLeft.style.top = '50%';
-    }
+    //页面滚动事件
+    document.addEventListener('scroll', function () {
+        //window.pageYOffset:获取页面被卷去的高度
+        if (window.pageYOffset >= mainTop) {
+            sidebarLeft.style.position = 'fixed'
+            sidebarLeft.style.top = sidebarLeftTop + 'px';
+        } else {
+            sidebarLeft.style.position = 'absolute'
+            sidebarLeft.style.top = '50%';
+        }
+    })
 })

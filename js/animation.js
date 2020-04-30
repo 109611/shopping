d9c3041aa0240e2b691744468789721a3a570621
XiaoMing <109611gg@gmail.com>
2020-04-30 17:34:45 +0800

    'upload'

---
+++ js/animation.js
@@ -0,0 +1,21 @@
+//缓动动画函数
+function animation(obj, target, callback) {
+    //callback = function (){}
+    //先把原来的定时器清除，保证一个元素只有一个定时器
+    clearInterval(obj.timer)
+    //给不同的元素添加不同名称的定时器
+    obj.timer = setInterval(function () {
+        if (obj.offsetLeft == target) {
+            clearInterval(obj.timer)
+            //判断是否有回调函数
+            if (callback) {
+                callback()
+            }
+        } else {
+            var step = (target - obj.offsetLeft) / 10
+            //设置步长：(目标值 - 现在的值) / 10 算出来的值要去掉小数(向上取整)
+            step = step > 0 ? Math.ceil(step) : Math.floor(step)
+            obj.style.left = obj.offsetLeft + step + 'px'
+        }
+    }, 15)
+}
--- js/index.js
+++ js/index.js
@@ -2,15 +2,60 @@
  * @Author: XiaoMing
  * @Date: 2020-04-29 22:54:02 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2020-04-30 14:51:23
+ * @Last Modified time: 2020-04-30 17:33:20
  */
 
 window.addEventListener('load', function () {
+    //焦点图 start
+    var focus = document.querySelector('.focus');
+    var focusImg = document.querySelector('.focusImg');
+    var focusPrev = document.querySelector('.focus_Prev');
+    var focusNext = document.querySelector('.focus_next');
+
+    //显示和隐藏左右切图按钮
+    focus.addEventListener('mouseenter', function () {
+        focusPrev.style.display = 'block';
+        focusNext.style.display = 'block';
+    });
+    focus.addEventListener('mouseleave', function () {
+        focusPrev.style.display = 'none';
+        focusNext.style.display = 'none';
+    })
+
+
+    //动态生成小圆点
+    var focusUl = document.createElement('ol');
+    for (let i = 0; i < focusImg.children.length; i++) {
+        let li = document.createElement('li');
+        li.classList.add('focus_index_item');
+        focusUl.appendChild(li);
+    }
+    focusUl.classList.add('focus_index');
+    focus.appendChild(focusUl);
+    focusUl.addEventListener('click', function (e) {
+        for (let i = 0; i < focusUl.children.length; i++) {
+            focusUl.children[i].className = 'focus_index_item';
+        }
+        e.target.classList.add('current');
+    })
+
+
+    //图片切换
+    focusPrev.addEventListener('click', function () {
+
+    });
+
+    focusNext.addEventListener('click', function () {
+
+    });
+    //焦点图模块 END
+
     //左侧边栏
     var sidebarLeft = document.querySelector('.sidebar_left');
     var main = document.querySelector('.main');
     var mainTop = main.offsetTop;   //获取main距离浏览器顶部的top值
     var sidebarLeftTop = sidebarLeft.offsetTop - mainTop;//等于sidebarLeft距离main顶部的值
+
 
     //页面滚动事件
     document.addEventListener('scroll', function () {
@@ -22,5 +67,5 @@ window.addEventListener('load', function () {
             sidebarLeft.style.position = 'absolute'
             sidebarLeft.style.top = '50%';
         }
-    })
+    });
 })
